a64eec05a924f973659c6ff9ba1f7dd6c4762ae3
rohxn-rai <rohan.rai932@outlook.com>
2024-12-13 09:32:11 -0500

    3rd commit .gitignore file updated(to ignore prettier) added moviecards(dynamic without extra filters)

--- moviedux/src/App.js
+++ moviedux/src/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 import "./styles.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import MovieGrid from "./components/MoviesGrid";
 
 function App() {
   return (
     <div className="App">
       <div className="container">
         <Header></Header>
+        <MovieGrid></MovieGrid>
       </div>
       <Footer></Footer>
     </div>
