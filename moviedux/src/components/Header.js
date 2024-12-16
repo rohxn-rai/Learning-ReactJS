bf1eff924e48807c56e0e5c24f566e874c608db3
rohxn-rai <rohan.rai932@outlook.com>
2024-12-16 04:38:55 -0500

    6th commit added filter options for search, genre and rating

--- moviedux/src/components/Header.js
+++ moviedux/src/components/Header.js
@@ -1,13 +1,11 @@
-import React from "react";
-import "../styles.css";
+import React from 'react';
+import '../styles.css';
 
 export default function Header() {
   return (
     <div className="header">
       <img src="logo.png" className="logo" alt="moviedux" />
-      <h2 className="app-subtitle">
-        It's time for popcorn! Find your next movie here.
-      </h2>
+      <h2 className="app-subtitle">It's time for popcorn! Find your next movie here.</h2>
     </div>
   );
 }
