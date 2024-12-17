e4bd16bc7cfd6d5307dfe7bbcd3bfbb85a93a49c
rohxn-rai <rohan.rai932@outlook.com>
2024-12-17 06:43:24 -0500

    12th commit added a ticket list to have a whole list of items added add ticket and delete tickets using a reducer mapped out the edit function with no functionality

--- bugblaster/src/App.js
+++ bugblaster/src/App.js
@@ -1,14 +1,31 @@
 import logo from "./logo.svg";
 import "./App.css";
 import "./styles.css";
+import { useReducer } from "react";
 import TicketForm from "./components/TicketForm";
+import ticketReducer from "./reducers/ticketReducer";
+import TicketList from "./components/TicketList";
 
 function App() {
+  const initialState = { tickets: [], editingTIcket: null };
+
+  const [state, dispatch] = useReducer(ticketReducer, initialState);
+
   return (
     <div className="App">
       <div className="container">
         <h1>Bug Blaster</h1>
-        <TicketForm />
+        <TicketForm dispatch={dispatch} />
+        {state.tickets.length > 0 && (
+          <div className="results">
+            <h2>All Tickets</h2>
+
+            <TicketList
+              tickets={state.tickets}
+              dispatch={dispatch}
+            ></TicketList>
+          </div>
+        )}
       </div>
     </div>
   );
